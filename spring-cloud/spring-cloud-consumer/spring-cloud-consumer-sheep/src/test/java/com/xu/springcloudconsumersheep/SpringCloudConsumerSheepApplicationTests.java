c2f41d3494a96cb01d5710e95e352cca09465fce
xuhongda <xuhongda7@outlook.com>
2018-06-21 16:40:20 +0800

    新增一个消费者，使用Hystrix Dashboard

---
+++ spring-cloud/spring-cloud-consumer/spring-cloud-consumer-sheep/src/test/java/com/xu/springcloudconsumersheep/SpringCloudConsumerSheepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xu.springcloudconsumersheep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConsumerSheepApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
