d190c2bd5d51b4a18ac043900d0738d82c2636c2
xuhongda <xuhongda7@outlook.com>
2018-08-06 09:10:42 +0800

    Rabbit mq 独立消费

---
+++ spring-eatmq/src/main/java/com/xu/springeatmq/SpringEatmqApplication.java
@@ -0,0 +1,12 @@
+package com.xu.springeatmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEatmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEatmqApplication.class, args);
+    }
+}
