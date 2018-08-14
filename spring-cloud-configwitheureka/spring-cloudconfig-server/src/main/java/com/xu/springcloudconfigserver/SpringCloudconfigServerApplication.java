0e6faf38fd8be4c4f3fd2ffd721829fec4106a30
xuhongda <xuhongda7@outlook.com>
2018-08-14 16:23:42 +0800

    spring cloud config with eureka

---
+++ spring-cloud-configwitheureka/spring-cloudconfig-server/src/main/java/com/xu/springcloudconfigserver/SpringCloudconfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.xu.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudconfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudconfigServerApplication.class, args);
+    }
+}
