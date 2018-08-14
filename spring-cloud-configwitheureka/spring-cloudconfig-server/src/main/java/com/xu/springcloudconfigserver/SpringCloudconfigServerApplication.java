4853552282abf6e2c7bf8c32d37e84e951c11d74
xuhongda <xuhongda7@outlook.com>
2018-08-14 17:19:41 +0800

    spring cloud config with eureka

--- spring-cloud-configwitheureka/spring-cloudconfig-server/src/main/java/com/xu/springcloudconfigserver/SpringCloudconfigServerApplication.java
+++ spring-cloud-configwitheureka/spring-cloudconfig-server/src/main/java/com/xu/springcloudconfigserver/SpringCloudconfigServerApplication.java
@@ -2,7 +2,14 @@ package com.xu.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author xuhongda
+ */
+@EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudconfigServerApplication {
 
