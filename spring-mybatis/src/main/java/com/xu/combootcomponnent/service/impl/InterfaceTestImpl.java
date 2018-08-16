c81f14f39572c495d5d6a9d27cdcdac87ab59495
xuhongda <xuhongda7@outlook.com>
2018-08-16 17:50:38 +0800

    spring boot 事务

--- spring-mybatis/src/main/java/com/xu/combootcomponnent/service/impl/InterfaceTestImpl.java
+++ spring-mybatis/src/main/java/com/xu/combootcomponnent/service/impl/InterfaceTestImpl.java
@@ -1,10 +1,12 @@
 package com.xu.combootcomponnent.service.impl;
 
+import com.xu.combootcomponnent.dao.UserDao;
 import com.xu.combootcomponnent.entity.UserEntity;
 import com.xu.combootcomponnent.mapper.mysql1Mapper.UserMapper;
 import com.xu.combootcomponnent.service.InterfaceTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author xuhongda on 2018/8/1
@@ -13,11 +15,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class InterfaceTestImpl implements InterfaceTest {
-    /*@Autowired
+    @Autowired
     private UserMapper userMapper;
+
+
     @Override
     public UserEntity getoneUser(long id) {
-       // return userMapper.getOne(1L);
-        return null;
-    }*/
+        return userMapper.getOne(1L);
+    }
+
+    @Override
+    @Transactional
+    public void modify(long id) {
+        UserEntity one = userMapper.getOne(id);
+        one.setUserName("vvvvvvvvv");
+        userMapper.update(one);
+        throw new RuntimeException();
+    }
+
+
 }
