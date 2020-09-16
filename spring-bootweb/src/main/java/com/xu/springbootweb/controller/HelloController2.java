997b215797b894ff8bbf938775ef4bf5be4b1c57
xuhongda <xuhongda7@outlook.com>
2020-09-16 15:40:57 +0800

    restTemplate

--- spring-bootweb/src/main/java/com/xu/springbootweb/controller/HelloController2.java
+++ spring-bootweb/src/main/java/com/xu/springbootweb/controller/HelloController2.java
@@ -1,7 +1,9 @@
 package com.xu.springbootweb.controller;
 
+import com.xu.pojo.Girl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -11,9 +13,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class HelloController2 {
+
+
+
     @ResponseBody
-    @GetMapping("hello2")
+    @GetMapping(value = "hello1")
+    public String hello1(String s1,String s2){
+        return s1+s2;
+    }
+
+    @ResponseBody
+    @PostMapping(value = "hello2",consumes = "application/x-www-form-urlencoded")
     public String hello2(String s1,String s2){
         return s1+s2;
     }
+
+
+    @ResponseBody
+    @GetMapping(value = "getGirl")
+    public Girl getGirl(String name){
+        Girl girl = new Girl();
+        girl.setAge("18");
+        girl.setName(name);
+        return girl;
+    }
+
+
+
 }
---
+++ spring-bootweb/src/test/java/com/xu/springbootweb/restTemplate/RestTemplateTest.java
@@ -0,0 +1,87 @@
+package com.xu.springbootweb.restTemplate;
+
+import com.xu.pojo.Girl;
+import org.junit.Test;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xuhongda on 2020/9/16
+ * com.xu.springbootweb.restTemplate
+ * spring-practice
+ */
+public class RestTemplateTest {
+
+    /**
+     *  RestTemplate post 请求
+     */
+    @Test
+    public void test001(){
+
+        //请求头
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        //请求参数
+        MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
+        parts.add("s1","hello");
+        parts.add("s2","world");
+
+
+        HttpEntity<MultiValueMap> httpEntity = new HttpEntity<>(parts,headers);
+        String url = "http://localhost:8017/hello2";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, httpEntity, String.class);
+
+        System.out.println(responseEntity.toString());
+        System.out.println(responseEntity.getBody());
+
+        // exchange 方法
+        ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
+        System.out.println(exchange.getBody());
+    }
+
+
+    @Test
+    public void test002(){
+
+
+        Map<String, String> vars = new HashMap<>();
+        vars.put("s1","hello");
+        vars.put("s2","world");
+        //uri 参数
+        String url = "http://localhost:8017/hello1?s1={s1}&s2={s2}";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url,String.class,vars);
+        System.out.println(result);
+    }
+
+
+
+
+
+    /**
+     *  restTemplate get 请求推荐写法
+     */
+
+    @Test
+    public void test003(){
+
+        //请求头
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap> httpEntity = new HttpEntity<>(headers);
+        //uri 参数
+        String url = "http://localhost:8017/getGirl?name={name}";
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Girl> exchange = restTemplate.exchange(url, HttpMethod.GET, httpEntity, Girl.class,"liu");
+        System.out.println(exchange.getBody().getName());
+
+    }
+}
