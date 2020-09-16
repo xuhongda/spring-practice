83550708a9f9d1945a18d1f2aa9296e7c9c1c47b
xuhongda <xuhongda7@outlook.com>
2020-09-16 17:03:36 +0800

    枚举类

--- spring-mybatis/src/main/java/com/xu/combootcomponnent/enums/UserSexEnum.java
+++ spring-mybatis/src/main/java/com/xu/combootcomponnent/enums/UserSexEnum.java
@@ -1,5 +1,25 @@
 package com.xu.combootcomponnent.enums;
 
 public enum UserSexEnum {
-	MAN, WOMAN
+
+
+	/**
+	 *  男
+	 */
+	MAN("男"),
+
+	/**
+	 *  女
+	 */
+	WOMAN("女");
+
+	UserSexEnum(String sex) {
+		this.sex = sex;
+	}
+
+	private String sex;
+
+	public String getSex() {
+		return sex;
+	}
 }
