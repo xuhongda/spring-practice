0a2840f860a77efb2c28677902b9ceb85a590b92
xuhongda <xuhongda7@outlook.com>
2020-12-03 13:55:06 +0800

    h2 database spring

--- spring-mvc/src/main/test/com/xu/test/H2Test.java
+++ spring-mvc/src/main/test/com/xu/test/H2Test.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author xuhongda on 2020/12/3
