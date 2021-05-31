ac6938962c25c44389b310e6e0a78072a21a6612
xuhongda <xuhongda7@outlook.com>
2021-05-31 16:07:05 +0800

    simplify

--- spring-cloud/spring-cloud-consumer/spring-cloud-consumer-sheep/src/main/java/com/xu/springcloudconsumersheep/remote/ChocolateRemote.java
+++ spring-cloud/spring-cloud-consumer/spring-cloud-consumer-sheep/src/main/java/com/xu/springcloudconsumersheep/remote/ChocolateRemote.java
@@ -8,32 +8,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * <p>
+ *
+ *  <p>
+ *   <p>@FeignClient</p> name:指定远程服务名称 fallback:服务调用失败断路
+ *   <P>@RequestMapping</P> value 与远程 Controller mapping名称对应
+ *    fallback指定断路类
+ *  </p>
  *
  * @author xuhongda on 2018/6/20
  * com.xu.springcloudconsumercat.remote
  * spring-practice
- * @FeignClient name:指定远程服务名称 fallback:服务调用失败断路
- * @RequestMapping value 与远程 Controller mapping名称对应
- * fallback指定断路类
- *
- * </p>
  */
 @FeignClient(name = "spring-cloud-producer-chocolate", fallback = ChocolateRemoteHystrix.class)
 public interface ChocolateRemote {
     /**
      * hello
-     *
      * @param str <p>@RequestParam 必须加value</p>
-     * @return
      */
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     String hello(@RequestParam(value = "str") String str);
 
     /**
      * 获取一个女孩
-     *
-     * @return
      */
     @RequestMapping(value = "girl", method = RequestMethod.GET)
     Girl meetGirl();
